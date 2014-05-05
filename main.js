239960e8be41f73daac9dc6cae8f61893a677337
drevilbones <drevilbones@gmail.com>
2014-05-05 00:14:21 -0500

    traditional hello world

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-var game = new Phaser.Game(500, 600, Phaser.AUTO, 'game_div');
+var game = new Phaser.Game(600, 600, Phaser.AUTO, 'game_div');
 
 var main_state = {
 
     preload: function() {},
 
-    create: function() {},
+    create: function() {
+
+        var text = "traditional hello world";
+        var style = {font: "24px Arial", fill: "#ff0044", align: "center" };
+
+        var t = game.add.text(150, game.world.centerY, text, style);
+
+    },
 
     update: function() {},
 
